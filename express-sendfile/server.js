559fe9499d89e60342d3096d5d8a549cd4b10048
tatukynsilehto <46740607+tatukynsilehto@users.noreply.github.com>
2021-04-19 21:28:38 +0300

    format

--- express-sendfile/server.js
+++ express-sendfile/server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/public', express.static(__dirname));
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'));
