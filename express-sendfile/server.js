4f70c8d912ed18664b259e21578549bcb4856572
tatukynsilehto <46740607+tatukynsilehto@users.noreply.github.com>
2021-04-29 18:32:16 +0300

    Update server.js

--- express-sendfile/server.js
+++ express-sendfile/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/public', express.static(__dirname + "/public&quot"));
+app.use('/public', express.static(__dirname + "/public"));
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'));
